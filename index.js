24437f7108963a601f0778076bf11ec06c8e09c4
Jay Chen <jscorpionse@yahoo.com>
2018-01-06 21:46:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // write your code below!
+<<<<<<< HEAD
 
 function happyHolidays (){
     return "Happy holidays!"
@@ -14,4 +15,10 @@ function happyHolidayTo(holiday, name){
 
 function holidayCountdown(holiday, number){
   return "It's 20 days until Mother's Day!"
+=======
+function happyHolidays(){
+
+  return ("Happy holidays!")
+
+>>>>>>> fb9be65c2819963d4d8589ae28a2559600dbe92b
 }
